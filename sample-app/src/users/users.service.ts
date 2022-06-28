26aec08c1c117f649b62739fba393645549c2f2d
TnIoP <pointblank94@naver.com>
2022-06-28 18:12:16 +0900

    feat: user api for nginx test

--- sample-app/src/users/users.service.ts
+++ sample-app/src/users/users.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly PrismaService: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.PrismaService.user.findMany();
   }
 
   findOne(id: number) {
